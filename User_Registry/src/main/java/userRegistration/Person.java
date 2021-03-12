f94013f8036970078e5086067ec6d192fc3d3d3e
Alfredo Gomez <alfredomgomez97@gmail.com>
2021-03-11 18:02:42 -0600

    Updated the names

--- User_Registry/src/main/java/userRegistration/Person.java
+++ User_Registry/src/main/java/userRegistration/Person.java
@@ -2,6 +2,45 @@ package userRegistration;
 
 public class Person {
 	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String bDay;
+	
+	public Person(int id, String firstName, String lastName, String bDay) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.bDay = bDay;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getbDay() {
+		return bDay;
+	}
+	public void setbDay(String bDay) {
+		this.bDay = bDay;
+	}
+	
+	
+	
 
 
 }
